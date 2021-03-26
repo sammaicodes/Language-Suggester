dd41aa7a3a2ef569c1f30628dd4efecbd4e89860
Sammai Gutierrez <sammaigutierrez@gmail.com>
2021-03-26 10:30:36 -0700

    Attempt to wite some logic with not much luck.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,29 @@ $(document).ready(function() {
   // when the button  is clicked
   $("#submit").submit(function() {
       
-   
+    // find which options were selected
+    // get all of the options
+    let option1 = $("#true1")[0].selected;
+    let option2 = $("#true2")[0].selected;
+    let option3 = $("#true3")[0].selected;
+    let option4 = $("#true4")[0].selected;
+    let option5 = $("#true5")[0].selected;
 
+    let option6 = $("#false1")[0].selected;
+    let option7 = $("#false2")[0].selected;
+    let option8 = $("#false3")[0].selected;
+    let option9 = $("#false4")[0].selected;
+    let option10 = $("#false5")[0].selected;
+
+    let answer;
+   
+    if (option1 === true) {
+      answer = "JavaScript";
+    } else if (option2 === true) {
+      answer = "C#";
+    } else if (option3 === true) {
+      answer = "Python";
+    } 
     // populate suggestion span
     $(".language").text(answer);
 
